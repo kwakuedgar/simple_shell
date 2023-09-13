2ac56aa897eb1977b7fadb89cbbcb03779e3698b
Edgar Nyamekye Agongo <kwakuedgar@gmail.com>
2023-09-13 10:54:31 -0700

    function that exits shell

---
+++ _edExit.c
@@ -0,0 +1,19 @@
+#include "edshell.h"
+
+/**
+*edExit - Function from shell
+*@ed_arg_s: arguments
+*Return: 0
+*/
+
+int edExit(char **ed_arg_s)
+{
+if (ed_arg_s[1])
+{
+return (atoi(ed_arg_s[1]));
+}
+else
+{
+return (0);
+}
+}
