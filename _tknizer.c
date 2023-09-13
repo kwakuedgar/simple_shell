b3357db9a548e2b9b4fa6b4d958b09efa1f5c658
Edgar Nyamekye Agongo <kwakuedgar@gmail.com>
2023-09-13 12:23:29 -0700

    function that tokenizes the input

---
+++ _tknizer.c
@@ -0,0 +1,46 @@
+include "edshell.h"
+
+/**
+ * tknizer - Tokenizes input
+ * @input: string input
+ * Return: pointer to array of string
+ */
+
+char **tknizer(char *input)
+{
+int i = 0;
+char **tkns;
+char *tkn;
+int size_BuFFer = 64;
+
+tkns = malloc(size_BuFFer * sizeof(char *));
+
+if (!tkns)
+{
+fprintf(stderr, "Token memory allocation failed\n");
+exit(EXIT_FAILURE);
+}
+tkn = strtok(input, ED_DELIM);
+while (tkn != NULL)
+{
+if (tkn[0] == '#')
+{
+break;
+}
+tkns[i] = tkn;
+i++;
+if (i >= size_BuFFer)
+{
+size_BuFFer += size_BuFFer;
+tkns = realloc(tkns, size_BuFFer *sizeof(char *));
+if (!tkns)
+{
+fprintf(stderr, "Token memory allocation failed");
+exit(EXIT_FAILURE);
+}
+}
+tkn = strtok(NULL, ED_DELIM);
+}
+tkns[i] = NULL;
+return (tkns);
+}
